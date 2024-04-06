02f55de22eed8c0d1e761d1298e286fb2fddc789
Nishal Barman <32856353+nishalbarman@users.noreply.github.com>
2024-04-06 21:32:18 +0530

    categories route pagination added

--- apps/server/routes/categories/category.routes.js
+++ apps/server/routes/categories/category.routes.js
@@ -2,12 +2,26 @@ const express = require("express");
 const router = express.Router();
 const Category = require("../../models/category.model");
 
-const TAG = "products/route.js:--";
+const TAG = "categories.routes.js:--";
 
 router.get("/", async (req, res) => {
   try {
-    const categories = await Category.find({});
-    return res.status(200).json({ data: categories });
+    const searchQuery = req.query;
+
+    const PAGE = searchQuery?.page || 1;
+    const LIMIT = searchQuery?.limit || 5;
+    const SKIP = (PAGE - 1) * LIMIT;
+
+    const totalCounts = await Category.countDocuments({});
+
+    const categories = await Category.find({})
+      .sort({ createdAt: "desc" })
+      .skip(SKIP)
+      .limit(LIMIT);
+
+    const totalPages = Math.ceil(totalCounts / LIMIT);
+
+    return res.status(200).json({ totalPages, data: categories });
   } catch (error) {
     console.log(TAG, error);
     return res
